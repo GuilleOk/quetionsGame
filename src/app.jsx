68e5c85bdb217caa1e89930e2f968e51766f98b5
GuilleOk <fernandez.martinez1403@gmail.com>
2024-10-21 18:25:18 -0400

    Camino preparado para mostrar una pregunta a la vez, falta crear el componente para mostrrla y sus posibles soluciones

--- src/app.jsx
+++ src/app.jsx
@@ -7,21 +7,29 @@ const App = () => {
   const [playerPoints, setPlayerPoints] = useState({ pointsPlayer1: 0, pointsPlayer2: 0 })
   // eslint-disable-next-line no-unused-vars
   const [winner, setWinner] = useState('')
+  const [finishTurn, setFinishTurn] = useState(false)
 
   const handlerReset = () => {
     setPlayerTurn(true)
     setPlayerPoints({ pointsPlayer1: 0, pointsPlayer2: 0 })
     setWinner('')
   }
+  const handlerSwitchTurn = () => {
+    if (finishTurn) {
+      setFinishTurn(false)
+      setPlayerTurn(!playerTurn)
+    }
+  }
   return (
     <div className='container my-5'>
       <h1 className='header mb-5'>In this game the players must answer five questions each other, at the end who had gone more points win the game</h1>
       <div className='d-flex containerPlayerTurn justify-content-center'>
         <h1>{playerTurn ? 'Player 1' : 'Player 2'}</h1>
-        <button className='btn btn-primary' onClick={() => setPlayerTurn(!playerTurn)}>Change Turn</button>
+        <button className='btn btn-primary' onClick={handlerSwitchTurn}>Change Turn</button>
         <button className='btn btn-outline-secondary' onClick={handlerReset}>Reset Game</button>
+        <button onClick={() => setFinishTurn(true)}>Terminar</button>
       </div>
-      <QuestionComponent />
+      <QuestionComponent setPlayerPoints={setPlayerPoints} playerTurn={playerTurn} finishTurn={finishTurn} setFinishTurn={setFinishTurn} winner={winner} setWinner={setWinner} />
     </div>
   )
 }
--- src/components/QuestionComponent.jsx
+++ src/components/QuestionComponent.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const QuestionComponent = () => {
+const QuestionComponent = ({ setPlayerPoints, playerTurn, finishTurn, setFinishTurn, winner, setWinner }) => {
+  const [questions, setQuestions] = useState([])
+  const [numberOfQuestionToShow, setNumberOfQuestionToShow] = useState(0)
+
+  useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple')
+      .then(response => response.json())
+      .then(data => {
+        // eslint-disable-next-line camelcase
+        const newQuestions = data.results.map(({ question, correct_answer, incorrect_answers }) => ({ question, correct_answer, incorrect_answers }))
+        setQuestions(newQuestions)
+        console.log(questions)
+      })
+  }, [winner])
+  useEffect(() => {
+    console.log(questions)
+    console.log(numberOfQuestionToShow)
+    if (finishTurn && questions.length !== 0) {
+      setNumberOfQuestionToShow(numberOfQuestionToShow + 1)
+      console.log('numberOfQuestionToShow: ', numberOfQuestionToShow)
+    }
+  }, [finishTurn])
+
   return (
-    <div>QuestionComponent</div>
+    <div>
+      <h1>{JSON.stringify(questions[numberOfQuestionToShow])}</h1>
+      hello
+    </div>
   )
 }
 
